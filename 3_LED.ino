93866f8b28f10e4c5ac4dcfb9c129d1e6bae9e52
JJrockstheMullet <sankalp.manjunath07@gmail.com>
2025-06-19 15:32:02 +0530

    Project 2 {Homework}

---
+++ 3_LED.ino
@@ -0,0 +1,67 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+   digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+   digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+   digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+   digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(8,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(2,HIGH);
+  delay(500);
+  digitalWrite(2,LOW);
+  delay(500);
+  digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+   digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500); digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500); digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500); digitalWrite(3,HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+}
